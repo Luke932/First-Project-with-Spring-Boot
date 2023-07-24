3a2778155d729ea2327bb71ba8a0c120aad6e7e0
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-07-24 14:40:30 +0200

    Complete #homework1

--- First_Project_With_SpringBoot/src/main/java/Luke932/First_Project_With_SpringBoot/FirstProjectWithSpringBootApplication.java
+++ First_Project_With_SpringBoot/src/main/java/Luke932/First_Project_With_SpringBoot/FirstProjectWithSpringBootApplication.java
@@ -3,10 +3,15 @@ package Luke932.First_Project_With_SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class FirstProjectWithSpringBootApplication {
 
 	public static void main(String[] args) {
+		// Esercizio1
+		log.info("Ciao Mondo");
 		SpringApplication.run(FirstProjectWithSpringBootApplication.class, args);
 	}
 
