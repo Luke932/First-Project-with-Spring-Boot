2b3c9848322988dae01c0e107ebe5b5b3c0646fa
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-07-24 14:27:45 +0200

    start project

---
+++ First_Project_With_SpringBoot/src/main/java/Luke932/First_Project_With_SpringBoot/FirstProjectWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Luke932.First_Project_With_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectWithSpringBootApplication.class, args);
+	}
+
+}
